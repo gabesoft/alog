9077f1b7f6e987b1e27b787ecc05ad87d2fd1a63
gabriel <gabesoft@gmail.com>
2012-02-18 18:26:04 -0800

    port updates

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 /**
- * Module dependencies.
- */
+* Module dependencies.
+*/
 
 var express = require('express')
-  , routes = require('./routes');
+, routes = require('./routes');
 
 var app = module.exports = express.createServer();
 
@@ -32,5 +32,6 @@ app.configure('production', function(){
 app.get('/', routes.index);
 
 var port = process.env.PORT || 3000
-app.listen(port);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
